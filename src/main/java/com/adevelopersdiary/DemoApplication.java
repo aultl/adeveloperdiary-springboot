03f06808f3abdfa1d803c43785b781347c9b9a8a
Les Ault <les.aut@gmail.com>
2017-04-05 19:42:20 -0400

    Fixed unknown HashMap error

--- src/main/java/com/adevelopersdiary/DemoApplication.java
+++ src/main/java/com/adevelopersdiary/DemoApplication.java
@@ -1,19 +1,20 @@
 package com.adevelopersdiary;
 
+import java.util.HashMap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-  public static HashMap<Long,Student> hmStudent;
+  public static HashMap<Long, Student> hmStudent;
 
 	public static void main(String[] args) {
-		hmStudent = new HashMap<Long,Student>();
+    hmStudent = new HashMap<Long, Student>();
 
 		Student one = new Student("John", "Math");
 		hmStudent.put(new Long(one.getId()), one);
 
-		Student two = new Student("","");
+		Student two = new Student("Jane","History");
 		hmStudent.put(new Long(two.getId()), two);
 	}
 }
