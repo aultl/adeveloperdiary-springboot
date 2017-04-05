7dfc6fb449ece2c0a2333b3c1f55d36a46114de7
Les Ault <les.aut@gmail.com>
2017-04-05 19:50:40 -0400

    Renamed files

--- src/main/java/com/adevelopersdiary/StudentService.java
+++ src/main/java/com/adevelopersdiary/StudentService.java
@@ -1,4 +1,4 @@
-package com.adevelopersdiary
+package com.adevelopersdiary;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
