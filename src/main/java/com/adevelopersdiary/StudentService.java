796b3f027db4a69df5477463c7b290d9d92af0f5
Les Ault <les.aut@gmail.com>
2017-04-05 22:09:27 -0400

    Added adStudent function

--- src/main/java/com/adevelopersdiary/StudentService.java
+++ src/main/java/com/adevelopersdiary/StudentService.java
@@ -9,8 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 class StudentService {
 
-   @RequestMapping(value="/rest/student",method = RequestMethod.GET)
+   @RequestMapping(value="/api/v1/student", method = RequestMethod.GET)
    public HashMap<Long,Student> getAllStudents(){
       return DemoApplication.hmStudent;
    }
+
+   @RequestMapping(value="/api/v1/student/add", method = RequestMethod.POST)
+   public Student addStudent(@RequestParam(value="name") String name,
+       @RequestParam(value="subject",defaultValue = "unknown") String subject){
+
+     Student student=new Student(name,subject);
+     DemoApplication.hmStudent.put(DemoApplication.counter.incrementAndGet(),student);
+     return student;
+  }
 }
